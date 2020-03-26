3bc01a2c400a93b098993ff1e158ea071e84fc28
root <root@localhost.localdomain>
2020-03-26 21:30:53 +0000

    Initialize board

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -1,2 +1,18 @@
 
 #!/bin/bash -x
+
+#Use case 1
+declare -a pos
+for (( i=0 ; i<9 ; i++ ))
+do
+	pos[$i]=$(($i + 1))
+done
+
+function board() {
+	echo " ${pos[0]} | ${pos[1]} | ${pos[2]}"
+	echo "-----------"
+	echo " ${pos[3]} | ${pos[4]} | ${pos[5]}"
+	echo "-----------"
+	echo " ${pos[6]} | ${pos[7]} | ${pos[8]}"
+}
+board
